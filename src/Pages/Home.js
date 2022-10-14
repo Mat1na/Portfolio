2fac9bfea2c78f8ce82ed9fe3df646c38d470868
Mat1na <Stamatia.Bampa@Student.intecbrussel.be>
2022-10-14 21:27:55 +0200

    updated

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Button, Col, Row } from 'react-bootstrap';
+import { Button, Col, Row, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import Typewriter from 'typewriter-effect';
 import { BsArrowDown } from 'react-icons/bs'
@@ -17,12 +17,12 @@ function Home() {
 
   return (
     <>
-      <Row className='head d-flex justify-content-center align-items-center' >
-        <Col md={6} ms={12} className="img-home-container d-flex justify-content-center align-items-center">
+      <Row className='head d-flex justify-content-center align-items-center p-5' >
+        <Col md={6} xs={12} className="img-home-container d-flex justify-content-center align-items-center">
           <img src='./images/me33.png' className='img-head' />
         </Col>
-        <Col md={6} ms={12} className="typewriter-effect-container ">
-          <div className='text-center'>
+        <Col md={6} xs={12} className="typewriter-effect-container ">
+          <div className='text-center typewriter'>
             <Typewriter
               onInit={(typewriter) => {
                 typewriter
@@ -39,27 +39,23 @@ function Home() {
           <div className='btn-container d-flex justify-content-center align-items-center pt-4'>
             {show && <a id="btn" href="/Contact"><span className="noselect">Say Hi!</span><div id="circle"></div></a>}
           </div>
-
-          {/* <Col md={12}>
-              <div className='btn-container pt-4'>
-                {show && <a id="btn" href="/Contact"><span className="noselect">Say Hi!</span><div id="circle"></div></a>}
-              </div>
-            </Col> */}
-
         </Col>
-
         <div className='box'></div>
-
-        {/* <div className='scroll-box'></div> */}
-
       </Row>
 
-      <Row className='section1'>
-        <div className="arrow bounce">
-          <a href="#"><BsArrowDown size={'2em'} fill='var(--Dark)' className="arrow p-1 rounded-1 mx-1" /></a>
+      < Container className='section1 d-flex justify-content-center align-items-center text-center'>
+        <div id="wrapper" >
+          <div id="wrapper-inner">
+            <Col xs={12} id="scroll-title" className='d-flex justify-content-center align-items-center text-center '>
+              Scroll down
+            </Col>
+            <Col md={12} id="scroll-down" className='d-flex justify-content-center align-items-center text-center'>
+              <span className="arrow-down">
+              </span>
+            </Col>
+          </div>
         </div>
-        <span className="arrow-text">Keep scrolling</span>
-      </Row>
+      </ Container>
 
       <Row className='section2 d-flex justify-content-center align-items-center'>
         <Col md={6} className='section2-text p-5'>
@@ -83,7 +79,6 @@ function Home() {
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sagittis purus quis enim porta, vitae dapibus augue viverra. Quisque in molestie est. Praesent ullamcorper tellus nec justo interdum, et tempor justo gravida. In quis consectetur felis.</p>
           <div className='btn3-container'><a id="btn" href='/projects'  ><span className="noselect">Take a look</span><div id="circle"></div></a></div>
         </Col >
-
       </Row>
 
 
