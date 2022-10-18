6443f83908e19ae1adbd5d2d5c63d67f6a2b7fd7
Mat1na <Stamatia.Bampa@Student.intecbrussel.be>
2022-10-18 21:58:42 +0200

    updated

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -1,12 +1,17 @@
 import React from 'react'
-import { Card, Container, ListGroup } from 'react-bootstrap'
-import { SiNetlify} from 'react-icons/si';
+import { Card, Col, Container, ListGroup, Row } from 'react-bootstrap'
+import { SiNetlify,SiVercel} from 'react-icons/si';
+import { FaInstagram, FaLinkedin, FaGithub, } from 'react-icons/fa'
+
+
 function Projects() {
   return (
    <>
    <Container>
-   <Card style={{ width: '18rem' }} className="card-projects">
-      <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+ <Row className='justify-content-center'>
+  <Col className='d-flex justify-content-center align-items-center'>
+ <Card style={{ width: '18rem' }} className="card-projects">
+      <Card.Img variant="top" src='./images/movieapp.jpg' />
       <Card.Body>
         <Card.Title>Card Title</Card.Title>
         <Card.Text>
@@ -14,16 +19,45 @@ function Projects() {
           bulk of the card's content.
         </Card.Text>
       </Card.Body>
-      <ListGroup className="list-group-flush">
-        <ListGroup.Item>Cras justo odio</ListGroup.Item>
-        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-      </ListGroup>
-      <Card.Body>
-        <Card.Link href="https://movieblender.netlify.app/"> <SiNetlify/>Movieblender</Card.Link>
-        <Card.Link href="#">Another Link</Card.Link>
+        <Card.Body>
+        <Card.Link href="https://movieblender.netlify.app/" target="_blank"> <SiNetlify/> Movieblender</Card.Link>
+        <Card.Link href="https://github.com/Mat1na/MovieApp" target="_blank"><FaGithub fill='var(--Dark)' /> Github</Card.Link>
       </Card.Body>
     </Card>
+    </Col>
+    <Col className='d-flex justify-content-center align-items-center'>
+    <Card style={{ width: '18rem' }} className="card-projects">
+      <Card.Img variant="top" src='./images/newsapp.jpg' />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+      </Card.Body>
+        <Card.Body>
+        <Card.Link href="https://the-today-news.vercel.app/country/US" target="_blank"> <SiVercel/> TheTodayNews</Card.Link>
+        <Card.Link href="https://github.com/Mat1na/NewsAppFrontForDeploy" target="_blank"><FaGithub fill='var(--Dark)' /> Github</Card.Link>
+      </Card.Body>
+    </Card>
+    </Col>
+    <Col className='d-flex justify-content-center align-items-center'>
+    <Card style={{ width: '18rem' }} className="card-projects">
+      <Card.Img variant="top" src='./images/movieapp.jpg' />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+      </Card.Body>
+        <Card.Body>
+        <Card.Link href="https://movieblender.netlify.app/" target="_blank"> <SiNetlify/> Movieblender</Card.Link>
+        <Card.Link href="https://github.com/Mat1na/MovieApp" target="_blank"><FaGithub fill='var(--Dark)' /> Github</Card.Link>
+      </Card.Body>
+    </Card>
+    </Col>
+ </Row>
    </Container>
    <Container></Container>
    </>
