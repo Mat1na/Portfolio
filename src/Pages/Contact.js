db9bbb9ba78a0a0d0487a9a645b8db5eae815e6a
Mat1na <Stamatia.Bampa@Student.intecbrussel.be>
2022-10-18 21:06:50 +0200

    updated

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -13,52 +13,51 @@ function Contact() {
 
     emailjs.sendForm('service_gi5govu', 'template_z0tk61g', form.current, 'PZWfLcE2WxSv3m0Dv')
       .then((result) => {
-          console.log(result.text);
-          e.target.reset()
+        console.log(result.text);
+        e.target.reset()
       }, (error) => {
-          console.log(error.text);
+        console.log(error.text);
       });
-    }
+  }
   return (
     <>
-    <div className='contact-page'>
-
-      <Row className='title-container pl-4'>
-
-        <div className='contact-me pb-5'>
-          Contact me
-        </div>
-
-        <Col md={12} className='pb-2'>
-        <a href="tel:+32478123137" className='mail-link'><BsPhoneVibrateFill size={'2em'} fill='var(--Light)' className="social-icon p-1 rounded-1 mx-1" />+32 478 12 31 37</a>
-        </Col>
-        <Col md={12}  className='pb-2'>
-          <a href='mailto:matina.bamba@gmail.com' subject="subject text" className='mail-link'><MdAlternateEmail size={'2em'} fill='var(--Light)' className="social-icon p-1 rounded-1 mx-1" />matina.bamba@gmail.com</a>
-        </Col>
-        <Col md={12}  className='pb-5'>
-          <p><BsPinMapFill size={'2em'} fill='var(--Light)' className="social-icon p-1 rounded-1 mx-1" />Dilbeek, Belgium</p>
-        </Col>
-      </Row>
-
-      
-      <Container className='contact-container'>
-        <div className='contact-bg'></div>
-        <Form className='form' ref={form} onSubmit={sendEmail}>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-            <Form.Label>Your Name</Form.Label>
-            <Form.Control name="user_name" type="text" placeholder="Enter your here..." />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control name="user_email" type="email" placeholder="name@example.com" />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-            <Form.Label>Write something</Form.Label>
-            <Form.Control as="textarea" name="message" rows={3} />
-          </Form.Group>
-          <Button as="input" type="submit" value="Submit" variant="outline-success w-100"/>
-        </Form>
-      </Container>
+      <div className='contact-page'>
+        <Row className='title-container pl-4'>
+          <div className='contact-me pb-4'>
+            Contact me
+          </div>
+          <h4 className='pb-4'>
+           Do you want to hire me or just say hi?<br/>
+           Feel free to send me an e-mail or call me! 👇
+          </h4>
+          <Col md={12} className='pb-2'>
+            <a href="tel:+32478123137" className='mail-link'><BsPhoneVibrateFill size={'2em'} fill='var(--Light)' className="social-icon p-1 rounded-1 mx-1" />+32 478 12 31 37</a>
+          </Col>
+          <Col md={12} className='pb-2'>
+            <a href='mailto:matina.bamba@gmail.com' subject="subject text" className='mail-link'><MdAlternateEmail size={'2em'} fill='var(--Light)' className="social-icon p-1 rounded-1 mx-1" />matina.bamba@gmail.com</a>
+          </Col>
+          <Col md={12} className='pb-5'>
+            <p><BsPinMapFill size={'2em'} fill='var(--Light)' className="social-icon p-1 rounded-1 mx-1" />Dilbeek, Belgium</p>
+          </Col>
+        </Row>
+        <Container className='contact-container'>
+          <div className='contact-bg'></div>
+          <Form className='form' ref={form} onSubmit={sendEmail}>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Your Name</Form.Label>
+              <Form.Control name="user_name" type="text" placeholder="Enter your name here..." />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control name="user_email" type="email" placeholder="name@example.com" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+              <Form.Label>Write something</Form.Label>
+              <Form.Control as="textarea" name="message" rows={3} />
+            </Form.Group>
+            <Button as="input" type="submit" value="Submit" variant="outline-success w-100" />
+          </Form>
+        </Container>
       </div>
     </>
   )
--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -1,8 +1,32 @@
 import React from 'react'
-
+import { Card, Container, ListGroup } from 'react-bootstrap'
+import { SiNetlify} from 'react-icons/si';
 function Projects() {
   return (
-    <div>Projects</div>
+   <>
+   <Container>
+   <Card style={{ width: '18rem' }} className="card-projects">
+      <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+      </Card.Body>
+      <ListGroup className="list-group-flush">
+        <ListGroup.Item>Cras justo odio</ListGroup.Item>
+        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+      </ListGroup>
+      <Card.Body>
+        <Card.Link href="https://movieblender.netlify.app/"> <SiNetlify/>Movieblender</Card.Link>
+        <Card.Link href="#">Another Link</Card.Link>
+      </Card.Body>
+    </Card>
+   </Container>
+   <Container></Container>
+   </>
   )
 }
 
