6fd0bf2ded85422dddc012ca7b83bc9cc96e79d3
Mat1na <Stamatia.Bampa@Student.intecbrussel.be>
2022-10-18 20:29:45 +0200

    updated

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,8 +1,66 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { Button, Col, Container, Form, Row } from 'react-bootstrap'
+import { BsPhoneVibrateFill, BsPinMapFill } from 'react-icons/bs'
+import { MdAlternateEmail } from 'react-icons/md'
+import emailjs from '@emailjs/browser';
 
 function Contact() {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_gi5govu', 'template_z0tk61g', form.current, 'PZWfLcE2WxSv3m0Dv')
+      .then((result) => {
+          console.log(result.text);
+          e.target.reset()
+      }, (error) => {
+          console.log(error.text);
+      });
+    }
   return (
-    <div>Contact</div>
+    <>
+    <div className='contact-page'>
+
+      <Row className='title-container pl-4'>
+
+        <div className='contact-me pb-5'>
+          Contact me
+        </div>
+
+        <Col md={12} className='pb-2'>
+        <a href="tel:+32478123137" className='mail-link'><BsPhoneVibrateFill size={'2em'} fill='var(--Light)' className="social-icon p-1 rounded-1 mx-1" />+32 478 12 31 37</a>
+        </Col>
+        <Col md={12}  className='pb-2'>
+          <a href='mailto:matina.bamba@gmail.com' subject="subject text" className='mail-link'><MdAlternateEmail size={'2em'} fill='var(--Light)' className="social-icon p-1 rounded-1 mx-1" />matina.bamba@gmail.com</a>
+        </Col>
+        <Col md={12}  className='pb-5'>
+          <p><BsPinMapFill size={'2em'} fill='var(--Light)' className="social-icon p-1 rounded-1 mx-1" />Dilbeek, Belgium</p>
+        </Col>
+      </Row>
+
+      
+      <Container className='contact-container'>
+        <div className='contact-bg'></div>
+        <Form className='form' ref={form} onSubmit={sendEmail}>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label>Your Name</Form.Label>
+            <Form.Control name="user_name" type="text" placeholder="Enter your here..." />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control name="user_email" type="email" placeholder="name@example.com" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+            <Form.Label>Write something</Form.Label>
+            <Form.Control as="textarea" name="message" rows={3} />
+          </Form.Group>
+          <Button as="input" type="submit" value="Submit" variant="outline-success w-100"/>
+        </Form>
+      </Container>
+      </div>
+    </>
   )
 }
 
