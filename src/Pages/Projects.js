30d3c18e5fb6a9a20388841cf6e4bff0417296b8
Mat1na <Stamatia.Bampa@Student.intecbrussel.be>
2022-10-18 22:50:15 +0200

    updated

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -7,11 +7,27 @@ import { FaInstagram, FaLinkedin, FaGithub, } from 'react-icons/fa'
 function Projects() {
   return (
    <>
-   <Container>
+   <Container className='web-sites-section'>
  <Row className='justify-content-center web-sites' > 
+ <Col className='d-flex justify-content-center align-items-center'>
+    <Card style={{ width: '18rem' }} className="card-projects">
+   <div className='img-hover-zoom'><Card.Img variant="top" className='project-img' src='./images/portfolio.jpg' /></div>
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+      </Card.Body>
+        <Card.Body>
+        <Card.Link href="https://my-portfolio-stamatia-bampa.vercel.app/" target="_blank"> <SiNetlify/> MyPortfolio</Card.Link>
+        <Card.Link href="https://github.com/Mat1na/Portfolio" target="_blank"><FaGithub fill='var(--Dark)' /> Github</Card.Link>
+      </Card.Body>
+    </Card>
+    </Col>
   <Col className='d-flex justify-content-center align-items-center'>
  <Card style={{ width: '18rem' }} className="card-projects">
-      <Card.Img variant="top" src='./images/movieapp.jpg' />
+ <div className='img-hover-zoom'><Card.Img variant="top" src='./images/movieapp.jpg' /></div>
       <Card.Body>
         <Card.Title>Card Title</Card.Title>
         <Card.Text>
@@ -27,7 +43,7 @@ function Projects() {
     </Col>
     <Col className='d-flex justify-content-center align-items-center'>
     <Card style={{ width: '18rem' }} className="card-projects">
-      <Card.Img variant="top" src='./images/newsapp.jpg' />
+    <div className='img-hover-zoom'><Card.Img variant="top" src='./images/newsapp.jpg' /></div>
       <Card.Body>
         <Card.Title>Card Title</Card.Title>
         <Card.Text>
@@ -38,22 +54,6 @@ function Projects() {
         <Card.Body>
         <Card.Link href="https://the-today-news.vercel.app/country/US" target="_blank"> <SiVercel/> TheTodayNews</Card.Link>
         <Card.Link href="https://github.com/Mat1na/NewsAppFrontForDeploy" target="_blank"><FaGithub fill='var(--Dark)' /> Github</Card.Link>
-      </Card.Body>
-    </Card>
-    </Col>
-    <Col className='d-flex justify-content-center align-items-center'>
-    <Card style={{ width: '18rem' }} className="card-projects">
-      <Card.Img variant="top" src='./images/movieapp.jpg' />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-      </Card.Body>
-        <Card.Body>
-        <Card.Link href="https://movieblender.netlify.app/" target="_blank"> <SiNetlify/> Movieblender</Card.Link>
-        <Card.Link href="https://github.com/Mat1na/MovieApp" target="_blank"><FaGithub fill='var(--Dark)' /> Github</Card.Link>
       </Card.Body>
     </Card>
     </Col>
