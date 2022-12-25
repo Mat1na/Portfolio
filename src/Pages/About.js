e7d682f394227db15f15720615ddaafd3abf616d
Mat1na <Stamatia.Bampa@Student.intecbrussel.be>
2022-12-25 14:45:54 +0100

    updated

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
 
 function About() {
   return (
-    <div className='about'>About</div>
+    <Container>
+    <div className='about'><p>My name is Stamatia Bampa and I am a <b> Junior Web Developer </b>just starting up and a <b>Graphic Designer</b>. I recently relocated from Athens
+    to Brussels, seeking to get insights into another cultural environment and improve myself professionally and
+    educationally.
+    I am conscious of the creativity around me, sometimes drawing inspiration from even the simplest daily activities. My biggest stimulation comes from art, cartoon movies and fashion which I incorporate in my work.
+    This portofilio is a collection of my personal concept projects created with my love and passion for all aspects
+    of graphic design.</p></div></Container>
   )
 }
 
