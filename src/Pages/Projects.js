47f09a198c899537d26601314a423991a53cd60a
Matina <matina.bampa@gmail.com>
2023-01-10 14:12:39 +0100

    updated

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import { Card, Col, Container, ListGroup, Row } from 'react-bootstrap'
+import { Card, Col, Container, Row } from 'react-bootstrap'
 import { SiNetlify, SiVercel } from 'react-icons/si';
-import { FaInstagram, FaLinkedin, FaGithub, } from 'react-icons/fa'
+import {FaGithub, } from 'react-icons/fa'
 
 
 function Projects() {
@@ -10,23 +10,23 @@ function Projects() {
       <Container className='web-sites-section'>
         <Row className='justify-content-center web-sites' >
           <Col className='d-flex justify-content-center align-items-center m-2'>
-            <Card style={{ width: '40rem',height:'40rem' }} className="card-projects me-4">
+            <Card style={{ width: '40rem', height: '40rem' }} className="card-projects me-4">
               <div className='img-hover-zoom'><Card.Img variant="top" className='project-img' src='./images/portfolio.jpg' /></div>
               <Card.Body>
                 <Card.Title>My Portfolio</Card.Title>
                 <Card.Text>
 
-                My portfolio was created as a React application <br/>
-              Technologies used:
-              <ul>
-                <li>React router</li>
-                <li>JSX</li>
-                <li>Bootstrap 5</li>
-                <li>CSS3</li>
-                <li>CSS animations</li>
-                <li>React Icons</li>
-                <li>npm pakcages: eg intersection-observer , typewriter-effect</li>
-              </ul> 
+                  My portfolio was created as a React application <br />
+                  Technologies used:
+                  <ul>
+                    <li>React router</li>
+                    <li>JSX</li>
+                    <li>Bootstrap 5</li>
+                    <li>CSS3</li>
+                    <li>CSS animations</li>
+                    <li>React Icons</li>
+                    <li>npm pakcages: eg intersection-observer , typewriter-effect</li>
+                  </ul>
 
                 </Card.Text>
               </Card.Body>
@@ -38,12 +38,12 @@ function Projects() {
 
           </Col>
           <Col className='d-flex justify-content-center align-items-center m-2'>
-            <Card style={{ width: '40rem',height:'40rem' }} className="card-projects">
+            <Card style={{ width: '40rem', height: '40rem' }} className="card-projects">
               <div className='img-hover-zoom'><Card.Img variant="top" src='./images/movieapp.jpg' /></div>
               <Card.Body>
                 <Card.Title>MovieBlender</Card.Title>
                 <Card.Text>
-                 MovieBlender is React project which works as a movie finder by genre catgories or custom search <br/>
+                  MovieBlender is React project which works as a movie finder by genre catgories or custom search <br />
                   Technologies used:
                   <ul>
                     <li>React router</li>
@@ -57,12 +57,12 @@ function Projects() {
               </Card.Body>
               <Card.Body>
                 <Card.Link href="https://movieblender.netlify.app/" target="_blank"> <SiNetlify />Visit Movieblender</Card.Link>
-                <Card.Link href="https://github.com/Mat1na/MovieApp" target="_blank"><FaGithub fill='var(--Dark)' />View on Github</Card.Link>
+                <Card.Link href="https://github.com/Mat1na/MovieApp" target="_blank"><FaGithub fill='var(--Dark)' />View source code on Github</Card.Link>
               </Card.Body>
             </Card>
           </Col>
           <Col className='d-flex justify-content-center align-items-center m-2'>
-            <Card style={{ width: '40rem',height:'50rem' }} className="card-projects">
+            <Card style={{ width: '40rem', height: '50rem' }} className="card-projects">
               <div className='img-hover-zoom'><Card.Img variant="top" src='./images/sGlobe.jpg' /></div>
               <Card.Body>
                 <Card.Title>sGlobe Lab</Card.Title>
@@ -77,19 +77,17 @@ function Projects() {
                     <li>bcrypt</li>
                     <li>JSON Web Token</li>
                     <li>HTTP Requests</li>
-                    <li>MongoDB</li>
                     <li>Bootstrap 5</li>
                     <li>CSS3</li>
                     <li>CSS animations</li>
-                    <li>React Icons</li>
                     <li>npm packages: eg intersection-observer , typewriter-effect, Lazy-load,hash-link</li>
                   </ul>
                 </Card.Text>
               </Card.Body>
               <Card.Body>
                 <Card.Link href="https://s-globe-front.vercel.app" target="_blank"> <SiVercel fill='var(--Dark)' />Visit sGlobe Lab</Card.Link><br />
-                <Card.Link href="hhttps://github.com/Mat1na/sGlobeFront" target="_blank"><FaGithub fill='var(--Dark)' />View Front-End code on Github</Card.Link>
-                <Card.Link href="https://github.com/Mat1na/sGlobeServer" target="_blank"><FaGithub fill='var(--Dark)' />View Back-End code on Github</Card.Link>
+                <Card.Link href="hhttps://github.com/Mat1na/sGlobeFront" target="_blank"><FaGithub fill='var(--Dark)' />View source code (Front-End )code on Github</Card.Link>
+                <Card.Link href="https://github.com/Mat1na/sGlobeServer" target="_blank"><FaGithub fill='var(--Dark)' />View source code (Back-End code) on Github</Card.Link>
               </Card.Body>
             </Card>
           </Col>
